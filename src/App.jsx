0d2f4622ee40993a8840445098fe34dee393865c
nahinhoque <abunahinibn@gmail.com>
2025-07-08 22:55:33 +0600

    I have update a setDate and Time in this project

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,16 @@ function App() {
   const [todos, setTodos] = useState([])
   const [showFinished, setshowFinished] = useState(false)
 
+  const [dateTime, setDateTime] = useState("");
+
   useEffect(() => {
     let todoString = localStorage.getItem("todos")
-    if(todoString){
-    let todos = JSON.parse(localStorage.getItem("todos"))
-    setTodos(todos)
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"))
+      setTodos(todos)
     }
   }, [])
-  
+
 
 
   const saveToLS = (params) => {
@@ -31,8 +33,8 @@ function App() {
   const toggleFinished = (e) => {
     setshowFinished(!showFinished)
   }
-  
-  
+
+
 
   const handleEdit = (e, id) => {
     let t = todos.filter(i => i.id === id)
@@ -60,7 +62,7 @@ function App() {
   const handleAdd = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
     setTodo("")
-   
+
     saveToLS()
 
   }
@@ -75,7 +77,7 @@ function App() {
     let index = todos.findIndex(item => {
       return item.id === id;
     })
-   
+
 
     let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
@@ -85,37 +87,48 @@ function App() {
 
 
 
+
+  const interval = setInterval(() => {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString();
+    const formattedTime = now.toLocaleTimeString();
+    setDateTime(`${formattedDate} - ${formattedTime}`);
+  }, 1000);
+
+
   return (
     <>
       <Navbar />
       <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 shadow-xl min-h-[80vh] md:w-[40%]">
-      <h1 className='font-bold text-center text-2xl'>TaskMaster - Your Daily Organizer</h1>
+        <h1 className='font-bold text-center text-2xl'>TaskMaster - Your Daily Organizer</h1>
+        
+        <h2 className='date-time font-bold  md:ml-[37%] ml-19 mt-4'>{dateTime}</h2>
+      
         <div className="addTodo my-5 flex flex-col gap-4">
           <h1 className='text-xl font-bold'> Add a Task<MdOutlineAddTask className='ml-28 mt-[-21px]' /></h1>
 
           <div className="flex">
-          <input onChange={handleChange} value={todo} placeholder="New task today..."  type="text" className='rounded-2xl px-5 w-full bg-white cursor-pointer' />
-          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-green-600 hover:bg-green-700 disabled:bg-indigo-600 hover:disabled:bg-indigo-700 px-4 py-2 m-3 text-lg text-white font-bold rounded-2xl cursor-pointer'>Save</button>
+            <input onChange={handleChange} value={todo} placeholder="New task today..." type="text" className='rounded-2xl px-5 w-full bg-white cursor-pointer' />
+            <button onClick={handleAdd} disabled={todo.length <= 3} className='bg-green-600 hover:bg-green-700 disabled:bg-indigo-600 hover:disabled:bg-indigo-700 px-4 py-2 m-3 text-lg text-white font-bold rounded-2xl cursor-pointer'>Save</button>
           </div>
-          
+
         </div>
 
-        
+
         <h1 className='text-xl font-bold'>My Tasks<FaArrowAltCircleDown className='ml-24 mt-[-20px]' /></h1>
-       
 
-        <div className="h-[1px] bg-black opacity-15 w-3/4 mx-auto my-4"></div>
 
-        <input onChange={toggleFinished} type="checkbox" className='ml-1 mt-4 cursor-pointer'  checked={showFinished} /> <span className='font-bold text-lg'>Show Finished</span>
+        
+        <input onChange={toggleFinished} type="checkbox" className='ml-1 mt-4 cursor-pointer' checked={showFinished} /> <span className='font-bold text-lg'>Show Finished</span>
 
         <div className="todos mx-5 my2">
-           {todos.length === 0 && <div className='m-8'>No task added!</div>}
+          {todos.length === 0 && <div className='m-8'>No task added!</div>}
           {todos.map(item => {
 
             return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex  my-5 justify-between ">
               <div className='flex gap-4'>
                 <input className='mt-[-1px] cursor-pointer' name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
-                <div className={item.isCompleted?"line-through" : ""}>{item.todo}</div>
+                <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
               </div>
 
               <div className="buttons mt-[-5px] flex h-full">
